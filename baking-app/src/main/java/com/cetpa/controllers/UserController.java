621b072af4af0cad9d196af65e33dcfcaf4970e0
Rahul7262 <rahulgupta.gt7262@gmail.com>
2023-11-05 02:09:07 +0530

    banking application project details

---
+++ baking-app/src/main/java/com/cetpa/controllers/UserController.java
@@ -0,0 +1,67 @@
+package com.cetpa.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cetpa.models.Account;
+import com.cetpa.models.User;
+import com.cetpa.services.UserService;
+
+@Controller
+@RequestMapping("cetpa-bank/user")
+public class UserController 
+{
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("login")
+	public String getLoginView()
+	{
+		return "user/user-login";
+	}
+	@RequestMapping("create-account")
+	public String getRegistrationView()
+	{
+		return "user/registration";
+	}
+	@RequestMapping("registerme")
+	public String registerUser(User user,Model model)
+	{
+		int accountNo=userService.createAccount(user);
+		model.addAttribute("an",accountNo);
+		model.addAttribute("name",user.getName());
+		return "user/registration-success";
+	}
+	@RequestMapping("authenticate")
+	public String authenticateUser(String userid,String password,Model model,HttpSession session)
+	{
+		User user=userService.getUser(userid);
+		model.addAttribute("userid",userid);
+		if(user==null)
+		{
+			model.addAttribute("msg","Userid does not exist");
+			return "user/user-login";
+		}
+		if(!user.getPassword().equals(password))
+		{
+			model.addAttribute("msg","Password is wrong");
+			return "user/user-login";
+		}
+		session.setMaxInactiveInterval(20);
+		int accountNo=userService.getAccount(userid);
+		session.setAttribute("name",user.getName());
+		session.setAttribute("accountNo",accountNo);
+		return "redirect:/cetpa-bank/home";
+	}
+	@RequestMapping("logout")
+	public String logoutUser(Model model,HttpSession session)
+	{
+		model.addAttribute("name",(String)session.getAttribute("name"));
+		session.invalidate();//code to destroy session object
+		return "user/logout";
+	}
+}
